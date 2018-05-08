7ccca6a494d4344409ac9eedbdd76b9984fe8779
Lila <lur159@psu.edu>
2018-05-08 12:43:47 -0400

    typo

--- minimds.py
+++ minimds.py
@@ -197,9 +197,9 @@ def main():
 	else:
 		prefix = ""
 	prefix1 = args.path1.split("/")[-1].split(".bed")[0]
-	structure1.write("{}/{}{}_structure.tsv".format(args.path1.split("/")[0:-1], prefix, prefix1)
+	structure1.write("{}/{}{}_structure.tsv".format(args.path1.split("/")[0:-1], prefix, prefix1))
 	prefix2 = args.path2.split("/")[-1].split(".bed")[0]
-	structure2.write("{}/{}{}_structure.tsv".format(args.path2.split("/")[0:-1], prefix, prefix2)
+	structure2.write("{}/{}{}_structure.tsv".format(args.path2.split("/")[0:-1], prefix, prefix2))
 
 if __name__ == "__main__":
 	main()
