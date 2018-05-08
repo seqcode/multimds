b46a6d130154ab875ea5ab789eecb3cb8ae009ee
Lila <lur159@psu.edu>
2018-05-08 10:43:00 -0400

    added smoothing parameter

--- relocalization_peaks.py
+++ relocalization_peaks.py
@@ -1,16 +1,12 @@
 import numpy as np
 import data_tools as dt
 import sys
-import compartment_analysis as ca
 import os
 import linear_algebra as la
 import array_tools as at
 from scipy import signal as sg
 from hmmlearn import hmm
 import argparse
-
-def normalize(values):
-	return np.array(values)/max(values)
 
 def call_peaks(data):
 	"""Calls peaks using Gaussian hidden markov model"""
@@ -45,10 +41,9 @@ def main():
 	parser = argparse.ArgumentParser(description="Identify locus-specific changes between Hi-C datasets")
 	parser.add_argument("path1", help="path to intrachromosomal Hi-C BED file 1")
 	parser.add_argument("path2", help="path to intrachromosomal Hi-C BED file 2")
-	parser.add_argument("activity_path1", help="path to active enrichment BED file 1")
-	parser.add_argument("activity_path2", help="path to active enrichment BED file 2")
 	parser.add_argument("-N", default=4, help="number of partitions")
 	parser.add_argument("-m", default=0, help="genomic coordinate of centromere")
+	parser.add_argument("-s", default=3, help="smoothing parameter for calling relocalization peaks")
 	args = parser.parse_args()
 
 	n = 5
@@ -99,49 +94,24 @@ def main():
 	#make structures compatible
 	dt.make_compatible((structure1, structure2))
 
-	#align
+	#tweak alignment
 	r, t = la.getTransformation(structure1, structure2)
 	structure1.transform(r,t)
 
-	#calculate error
 	coords1 = np.array(structure1.getCoords())
 	coords2 = np.array(structure2.getCoords())
 	dists = [la.calcDistance(coord1, coord2) for coord1, coord2 in zip(coords1, coords2)]
 	print np.mean(dists)
 
-	#compartments
-	contacts1 = dt.matFromBed(path1, structure1)
-	contacts2 = dt.matFromBed(path2, structure2)
-	at.makeSymmetric(contacts1)
-	at.makeSymmetric(contacts2)
-
-	compartments1 = ca.get_compartments(contacts1, structure1, args.activity_path1, chrom, res_kb), True)
-	compartments2 = ca.get_compartments(contacts2, structure2, args.activity_path2, chrom, res_kb), True)	
-
-	gen_coords = structure1.getGenCoords()
-
-	dists = normalize(dists)
-	compartment_diffs = np.abs(compartments1 - compartments2)
-	compartment_diffs = normalize(compartment_diffs)
-
-	smoothed_dists = sg.cwt(dists, sg.ricker, [smoothing_parameter])[0]
-	smoothed_diffs = sg.cwt(compartment_diffs, sg.ricker, [smoothing_parameter])[0]
+	smoothed_dists = sg.cwt(dists, sg.ricker, [args.smoothing_parameter])[0]
 	dist_peaks = call_peaks(smoothed_dists)
-	diff_peaks = call_peaks(smoothed_diffs)
 
-	with open("dist_peaks.bed", "w") as out:
+	with open("{}_{}_relocalization.bed".format(prefix1, prefix2), "w") as out:
 		for peak in dist_peaks:
 			start, end = peak
 			peak_dists = dists[start:end]
 			max_dist_index = np.argmax(peak_dists) + start
 			out.write("\t".join(("chr{}".format(chrom), str(gen_coords[start]), str(gen_coords[end]), str(gen_coords[max_dist_index]), str(compartments1[max_dist_index]), str(compartments2[max_dist_index]))))
-			out.write("\n")
-		out.close()
-
-	with open("comp_peaks.bed", "w") as out:
-		for peak in diff_peaks:
-			start, end = peak
-			out.write("\t".join(("chr{}".format(chrom), str(gen_coords[start]), str(gen_coords[end]))))
 			out.write("\n")
 		out.close()
 
