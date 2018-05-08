4b7f072a7c34a3cd80e4fdd1063437db63d0bda7
Lila <lur159@psu.edu>
2018-05-08 10:25:46 -0400

    added scripts for testing

---
+++ scripts/get_hic_data.sh
@@ -0,0 +1,30 @@
+set -e
+
+CELL_TYPE=$1
+
+mkdir -p hic_data
+
+cd hic_data
+
+if [ ! -e $CELL_TYPE"_"*_100kb.bed ]
+	then
+		if [ ! -e GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz ]
+			then
+				wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE63nnn/GSE63525/suppl/GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
+		fi
+
+		tar xzf GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz $CELL_TYPE/100kb_resolution_intrachromosomal
+	
+		for CHROM in 1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19 20 21 22	#skip chr9
+		do
+			echo $CHROM
+			python normalize.py $CELL_TYPE 100000 $CHROM
+		done
+			
+		rm GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
+
+		cat $CELL_TYPE"_22_100kb".bed | awk '$2 >= 24000000 && $5 >= 24000000 {print $0}' > tmp	#filter out translocation
+		mv tmp $CELL_TYPE"_22_100kb".bed
+fi	
+
+cd ..
---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+set -e
+
+./get_hic_data.sh GM12878_combined
+./get_hic_data.sh K562
