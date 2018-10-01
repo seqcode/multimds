ba24d452a9651750f1ea73faa18fcdb9426dc3d6
Lila <lur159@psu.edu>
2018-10-01 17:00:11 -0400

    updated script

--- scripts/relocalization_peaks.py
+++ scripts/relocalization_peaks.py
@@ -8,6 +8,7 @@ import linear_algebra as la
 import array_tools as at
 from scipy import signal as sg
 from hmmlearn import hmm
+from scipy import stats as st
 
 def normalize(values):
 	return np.array(values)/max(values)
@@ -55,57 +56,15 @@ chrom = sys.argv[3]
 centromere = sys.argv[4]
 num_partitions = sys.argv[5]
 smoothing_parameter = float(sys.argv[6])
-n = 1
 
 path1 = "/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}kb_filtered.bed".format(cell_type1, chrom, res_kb)
 path2 = "/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}kb_filtered.bed".format(cell_type2, chrom, res_kb)
 
-min_error = sys.float_info.max
-for iteration in range(n):
-	os.system("python ../multimds.py -m {} -N {} -o {}_ {} {}".format(centromere, num_partitions, iteration, path1, path2))
-		
-	#load structures
-	structure1 = dt.structure_from_file("/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}_{}kb_filtered_structure.tsv".format(iteration, cell_type1, chrom, res_kb))	
-	structure2 = dt.structure_from_file("/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}_{}kb_filtered_structure.tsv".format(iteration, cell_type2, chrom, res_kb))
+os.system("python ../multimds.py --full {} {}".format(path1, path2))
 
-	#rescale
-	structure1.rescale()
-	structure2.rescale()
-
-	#make structures compatible
-	dt.make_compatible((structure1, structure2))
-
-	#align
-	r, t = la.getTransformation(structure1, structure2)
-	structure1.transform(r,t)
-
-	#calculate error
-	coords1 = np.array(structure1.getCoords())
-	coords2 = np.array(structure2.getCoords())
-	error = np.mean([la.calcDistance(coord1, coord2) for coord1, coord2 in zip(coords1, coords2)])
-	if error < min_error:
-		min_error = error
-		best_iteration = iteration
-
-for iteration in range(n):
-	if iteration == best_iteration:
-		#load structures
-		structure1 = dt.structure_from_file("/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}_{}kb_filtered_structure.tsv".format(iteration, cell_type1, chrom, res_kb))	
-		structure2 = dt.structure_from_file("/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}_{}kb_filtered_structure.tsv".format(iteration, cell_type2, chrom, res_kb))
-	else:
-		os.system("rm /data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}_{}kb_filtered_structure.tsv".format(iteration, cell_type1, chrom, res_kb))	
-		os.system("rm /data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}_{}kb_filtered_structure.tsv".format(iteration, cell_type2, chrom, res_kb))	
-
-#rescale
-structure1.rescale()
-structure2.rescale()
-
-#make structures compatible
-dt.make_compatible((structure1, structure2))
-
-#align
-r, t = la.getTransformation(structure1, structure2)
-structure1.transform(r,t)
+#load structures
+structure1 = dt.structure_from_file("/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}kb_filtered_structure.tsv".format(cell_type1, chrom, res_kb))	
+structure2 = dt.structure_from_file("/data/drive1/test/archive/multimds/scripts/hic_data/{}_{}_{}kb_filtered_structure.tsv".format(cell_type2, chrom, res_kb))
 
 #calculate error
 coords1 = np.array(structure1.getCoords())
@@ -119,15 +78,12 @@ contacts2 = dt.matFromBed(path2, structure2)
 at.makeSymmetric(contacts1)
 at.makeSymmetric(contacts2)
 
-enrichments = np.array(np.loadtxt("binding_data/Gm12878_{}_100kb_active_coverage.bed".format(chrom), dtype=object)[:,6], dtype=float)
-bin_nums = structure1.nonzero_abs_indices() + structure1.chrom.minPos/structure1.chrom.res
-enrichments = enrichments[bin_nums]
-compartments1 = np.array(ca.get_compartments(contacts1, enrichments))
+compartments1 = np.array(ca.get_compartments(contacts1))
+compartments2 = np.array(ca.get_compartments(contacts2))
 
-enrichments = np.array(np.loadtxt("binding_data/K562_{}_100kb_active_coverage.bed".format(chrom), dtype=object)[:,6], dtype=float)
-bin_nums = structure2.nonzero_abs_indices() + structure2.chrom.minPos/structure2.chrom.res
-enrichments = enrichments[bin_nums]
-compartments2 = np.array(ca.get_compartments(contacts2, enrichments))
+r, p = st.pearsonr(compartments1, compartments2)
+if r < 0:
+	compartments2 = -compartments2
 
 gen_coords = structure1.getGenCoords()
 
