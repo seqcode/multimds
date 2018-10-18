a0a1e01cb6f4520fd0ab03e9cebe42d382ac7363
Lila <lur159@psu.edu>
2018-10-18 11:58:37 -0400

    updated script

--- scripts/get_hic_data.sh
+++ scripts/get_hic_data.sh
@@ -6,27 +6,27 @@ mkdir -p hic_data
 
 cd hic_data
 
-if [ ! -e $CELL_TYPE"_"*_100kb.bed ]
+if [ ! -d $CELL_TYPE/100kb_resolution_intrachromosomal ]
 	then
 		if [ ! -e GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz ]
 			then
 				wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE63nnn/GSE63525/suppl/GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
 		fi
-		tar xzf GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz $CELL_TYPE/100kb_resolution_intrachromosomal
-	
-		for CHROM in `seq 22`
-		do
-			echo $CHROM
-			if [ -d $CELL_TYPE/100kb_resolution_intrachromosomal/chr$CHROM ]
-				then
-					python ../normalize.py $CELL_TYPE 100000 $CHROM
-			fi
-		done
-			
-		rm GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
+	tar xzf GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz $CELL_TYPE/100kb_resolution_intrachromosomal
+fi
 
-		cat $CELL_TYPE"_22_100kb".bed | awk '$2 >= 24000000 && $5 >= 24000000 {print $0}' > tmp	#filter out translocation
-		mv tmp $CELL_TYPE"_22_100kb".bed
-fi	
+for CHROM in `seq 22`
+do
+	echo $CHROM
+	if [ -d $CELL_TYPE/100kb_resolution_intrachromosomal/chr$CHROM ] && [ ! -e ${CELL_TYPE}_${CHROM}_100kb.bed ]
+		then
+			python ../normalize.py $CELL_TYPE 100000 $CHROM
+	fi
+done
+	
+#rm GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
+
+cat $CELL_TYPE"_22_100kb".bed | awk '$2 >= 24000000 && $5 >= 24000000 {print $0}' > tmp	#filter out translocation
+mv tmp $CELL_TYPE"_22_100kb".bed
 
 cd ..
