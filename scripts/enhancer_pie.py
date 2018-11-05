acb0615a016db5fa42667a81df7f463de54cf32a
Lila <lur159@psu.edu>
2018-11-05 18:21:42 -0500

    fixed notation

--- scripts/enhancer_pie.py
+++ scripts/enhancer_pie.py
@@ -3,8 +3,8 @@ import sys
 
 plt.pie((int(sys.argv[1]), int(sys.argv[2])), labels=("Enhancer", "No enhancer"))
 plt.title("Relocalization peaks")
-plt.savefig("relocalization_superenhancer_pie")
+plt.savefig("relocalization_enhancer_pie")
 plt.close()
 plt.pie((int(sys.argv[3]), int(sys.argv[4])), labels=("Enhancer", "No enhancer"))
 plt.title("Background A compartment")
-plt.savefig("background_superenhancer_pie")
+plt.savefig("background_enhancer_pie")
