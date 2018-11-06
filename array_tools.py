9299073f9f14da8ca2ba3f2fe765d6b375f9d12b
Lila <lur159@psu.edu>
2018-11-06 11:36:00 -0500

    debug

--- array_tools.py
+++ array_tools.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pymp
 
 def contactToDist(contactMat, alpha):
 	"""Convert contact matrix to distance matrix."""
@@ -11,21 +10,8 @@ def contactToDist(contactMat, alpha):
 				distMat[i,j] = contactMat[i,j]**(-1./alpha)
 	return distMat
 
-def makeSymmetric(mat, num_threads):
+def makeSymmetric(mat):
 	"""Make below-diagonal matrix symmetric, in place"""
-	n = len(mat)
-	pymp_mat = pymp.shared.array((n,n))
-	pymp_mat[0:n,0:n] = mat[0:n,0:n]	#initialize with data
-
-	partition_size = n/num_threads
-	with pymp.Parallel(num_threads) as p:
-                for thread_num in p.range(num_threads):
-                        start = partition_size*thread_num
-                        if thread_num == num_threads-1:
-                                end = n
-                        else:
-                                end = partition_size*(thread_num+1)
-                        for i in range(start, end):
-				for j in range(i):
-					pymp_mat[j,i] = pymp_mat[i,j]
-	return pymp_mat
+	for row in range(len(mat)):
+		for col in range(row):
+			mat[col,row] = mat[row,col]
