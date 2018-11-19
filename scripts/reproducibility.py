ba5314ceace716a77688077673df6d6bfd05e24f
Lila <lur159@psu.edu>
2018-11-19 17:59:54 -0500

    updated reproducibility

--- scripts/reproducibility.py
+++ scripts/reproducibility.py
@@ -7,11 +7,12 @@ from matplotlib import pyplot as plt
 from scipy import stats as st
 import os
 
-chrom = 21
-res_kb = 100
-exp_names = ("GM12878_combined", "K562")
-path1 = "hic_data/{}_{}_{}kb.bed".format(exp_names[0], chrom, res_kb)
-path2 = "hic_data/{}_{}_{}kb.bed".format(exp_names[1], chrom, res_kb)
+path1 = sys.argv[1]
+path2 = sys.argv[2]
+
+prefix1 = path1.split(".")[0]
+prefix2 = path2.split(".")[0]
+
 n = 10
 
 all_r_sq = []
@@ -24,8 +25,8 @@ for p in ps:
 		print(i)
 		os.system("python ../multimds.py -P {} --full {} {}".format(p, path1, path2))
 
-		structure1 = dt.structure_from_file("{}_{}_{}kb_structure.tsv".format(exp_names[0], chrom, res_kb))
-		structure2 = dt.structure_from_file("{}_{}_{}kb_structure.tsv".format(exp_names[1], chrom, res_kb))
+		structure1 = dt.structure_from_file("{}_structure.tsv".format(prefix1))
+		structure2 = dt.structure_from_file("{}_structure.tsv".format(prefix2))
 		
 		if p == 0:
 			r, t = la.getTransformation(structure1, structure2)
@@ -42,8 +43,6 @@ for p in ps:
 	all_r_sq.append(r_sq)
 
 medianprops = dict(linestyle="none")
-<<<<<<< HEAD
-<<<<<<< HEAD
 plt.subplot2grid((10,10), (0,0), 9, 10, frameon=False)
 plt.boxplot(all_r_sq, notch=True, patch_artist=True, labels=ps, medianprops=medianprops)
 plt.ylabel("Correlation between iterations", fontsize=14)
@@ -52,17 +51,5 @@ plt.axis([x_start, x_end, y_start, y_end], frameon=False)
 plt.axvline(x=x_start, color="k", lw=4)
 plt.axhline(y=y_start, color="k", lw=6)	
 plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=5, labelsize=10)
-<<<<<<< HEAD
-plt.savefig("{}_{}_reproducibility".format(celltype1, celltype2))
+plt.savefig("{}_{}_reproducibility".format(prefix1, prefix2))
 plt.show()
-=======
-=======
->>>>>>> parent of 951c14b... improved fig
-plt.boxplot(all_r_sq, notch=True, patch_artist=True, labels=ps, medianprops=medianprops, showfliers=False)
-plt.ylabel("Correlation between iterations", fontsize=15)
-plt.xlabel("Difference penalty", fontsize=15)
-plt.savefig("reproducibility")
->>>>>>> parent of 951c14b... improved fig
-=======
-plt.savefig("reproducibility")
->>>>>>> parent of d41ee39... saving unmerged files
