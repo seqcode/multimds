566f87d346ed8107f15c8cf613b0b286ea8d6189
Lila <lur159@psu.edu>
2018-12-21 12:08:36 -0500

    removed K562 chr22

--- scripts/get_hic_data.sh
+++ scripts/get_hic_data.sh
@@ -8,27 +8,29 @@ mkdir -p hic_data
 
 cd hic_data
 
-if [ ! -d $CELL_TYPE/${RES}kb_resolution_intrachromosomal ]
+if [ ! -d $CELL_TYPE/${RES_KB}kb_resolution_intrachromosomal ]
 	then
 		if [ ! -e GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz ]
 			then
 				curl ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE63nnn/GSE63525/suppl/GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz -o GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
 		fi
-	tar xzf GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz $CELL_TYPE/${RES}kb_resolution_intrachromosomal
+	tar xzf GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz $CELL_TYPE/${RES_KB}kb_resolution_intrachromosomal
 fi
 
-for CHROM in `seq 22`
+if [ $CELL_TYPE == "K562" ]
+	then
+		CHROMS=`seq 21`		#skip philadelphia chromosome
+	else
+		CHROMS=`seq 22`
+fi
+
+for CHROM in $CHROMS
 do
 	echo $CHROM
-	if [ -d $CELL_TYPE/${RES}kb_resolution_intrachromosomal/chr$CHROM ] && [ ! -e ${CELL_TYPE}_${CHROM}_${RES}kb.bed ]
+	if [ -d $CELL_TYPE/${RES_KB}kb_resolution_intrachromosomal/chr$CHROM ] && [ ! -e ${CELL_TYPE}_${CHROM}_${RES_KB}kb.bed ]
 		then
 			python ../normalize.py $CELL_TYPE $RES $CHROM
 	fi
 done
 	
-#rm GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
-
-cat $CELL_TYPE"_22_"${RES}kb.bed | awk '$2 >= 24000000 && $5 >= 24000000 {print $0}' > tmp	#filter out translocation
-mv tmp $CELL_TYPE"_22_"${RES}kb.bed
-
 cd ..
