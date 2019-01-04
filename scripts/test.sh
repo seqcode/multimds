9c85db045fbfd8440df95df01f8f586a2fdd8d3e
Lila <lur159@psu.edu>
2019-01-04 11:56:31 -0500

    updated test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,15 @@
 set -e
 
-./get_hic_data.sh GM12878_combined
-./get_hic_data.sh K562
+./get_hic_data.sh GM12878_combined 100000
+./get_hic_data.sh K562 100000
 
-python ../multimds.py --full hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
-python ../multimds.py -N 2 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
-python ../multimds.py -l 5 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+python ../multimds.py hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+python ../multimds.py -P 0.1 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+python ../multimds.py -w 0.1 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+python ../multimds.py --partitioned hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+python ../multimds.py --partitioned -N 2 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+python ../multimds.py --partitioned -l 5 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
 python ../multimds.py -n 1 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
 python ../multimds.py -a 3 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
-python ../multimds.py -m 28000000 hic_data/GM12878_combined_20_100kb.bed hic_data/K562_20_100kb.bed
+python ../multimds.py -o test_ hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+python ../multimds.py --partitioned -r 28000000 hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
