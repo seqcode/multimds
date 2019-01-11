21910355714c6faf413a103b5102350559015707
Lila <lur159@psu.edu>
2019-01-11 11:00:01 -0500

    debug

--- scripts/get_hic_data.sh
+++ scripts/get_hic_data.sh
@@ -8,7 +8,7 @@ mkdir -p hic_data
 
 cd hic_data
 
-if [ ! -d $CELL_TYPE/${RES}kb_resolution_intrachromosomal ]
+if [ ! -d $CELL_TYPE/${RES_KB}kb_resolution_intrachromosomal ]
 	then
 		if [ ! -e GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz ]
 			then
@@ -19,16 +19,11 @@ fi
 
 for CHROM in `seq 22`
 do
-	echo $CHROM
 	if [ -d $CELL_TYPE/${RES_KB}kb_resolution_intrachromosomal/chr$CHROM ] && [ ! -e ${CELL_TYPE}_${CHROM}_${RES_KB}kb.bed ]
 		then
+			echo $CHROM
 			python ../normalize.py $CELL_TYPE $RES $CHROM
 	fi
 done
 	
-#rm GSE63525_$CELL_TYPE"_intrachromosomal_contact_matrices".tar.gz
-
-cat $CELL_TYPE"_22_"${RES_KB}kb.bed | awk '$2 >= 24000000 && $5 >= 24000000 {print $0}' > tmp	#filter out translocation
-mv tmp $CELL_TYPE"_22_"${RES_KB}kb.bed
-
 cd ..
