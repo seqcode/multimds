46244d2a58943dcd232a0c9877b7b4e6bf8a6d46
Lila <lur159@psu.edu>
2019-01-15 10:33:40 -0500

    sup5

---
+++ scripts/sup5.sh
@@ -0,0 +1,9 @@
+set -e
+
+for CONDITION in ctrl galactose
+do
+	cat ${CONDITION}_Scer_12_32kb.bed | awk '$2 < 448000 && $5 < 448000 {print $0}' > ${CONDITION}_Scer_12-upstream_32kb.bed
+	cat ${CONDITION}_Scer_12_32kb.bed | awk '$2 >= 448000 && $5 >= 448000 {print $0}' > ${CONDITION}_Scer_12-downstream_32kb.bed
+done
+
+python sup5.py
