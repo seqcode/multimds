27f645f6f5eec0630e21e96dc3731a299f032add
Lila <lur159@psu.edu>
2019-01-15 12:54:03 -0500

    added download

--- scripts/sup5.sh
+++ scripts/sup5.sh
@@ -1,5 +1,7 @@
 set -e
 
+./get_yeast_data.sh
+
 for CONDITION in ctrl galactose
 do
 	cat ${CONDITION}_Scer_12_32kb.bed | awk '$2 < 448000 && $5 < 448000 {print $0}' > ${CONDITION}_Scer_12-upstream_32kb.bed
