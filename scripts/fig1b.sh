8fc7e031a34a3f13348c1ecc4451be32c15ae3b4
Lila <lur159@psu.edu>
2019-03-04 12:06:08 -0500

    paths

--- scripts/fig1b.sh
+++ scripts/fig1b.sh
@@ -3,4 +3,4 @@ set -e
 ./get_hic_data.sh GM12878_combined 100000
 ./get_hic_data.sh K562 100000
 
-python reproducibility.py GM12878_combined_21_100kb.bed K562_21_100kb.bed
+python reproducibility.py hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
