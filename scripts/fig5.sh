4013f31696e714cab971b5c58bef40675a079448
Lila <lur159@psu.edu>
2019-03-08 19:44:00 -0500

    uncommented

--- scripts/fig5.sh
+++ scripts/fig5.sh
@@ -4,21 +4,21 @@ set -e
 ./process_cohesin-wt.sh
 ./process_cohesin-ko.sh
 echo "hepatocyte-cohesin-KO hepatocyte-WT" > cohesin_design.txt
-python quantify_z.py 20 cohesin_design.txt 0.04
+python quantify_z.py 20 cohesin_design.txt 0.04 0.75 cohesin depletion
 
 #Brd2
 
 RES=100000
 ./process_g1e.sh WT $RES
-./process_g1e.sh KO-rep1 $RES D_BRD2KO_
-./process_g1e.sh KO-rep2 $RES C8_BRD2KO_
+/process_g1e.sh KO-rep1 $RES D_BRD2KO_
+#./process_g1e.sh KO-rep2 $RES C8_BRD2KO_
 
 echo "KO-rep1-G1E KO-rep2-G1E" > brd2_rep_design.txt
 echo "KO-rep1-G1E WT-G1E" > brd2_design.txt
 echo "KO-rep2-G1E WT-G1E" >> brd2_design.txt
 
-python quantify_z.py 20 brd2_design.txt 0.02
-python quantify_z.py 20 brd2_rep_design.txt 0.035
+python quantify_z.py 20 brd2_design.txt 0.02 0.75 Brd2 depletion
+#python quantify_z.py 20 brd2_rep_design.txt 0.035
 
 #CTCF
 ./process_ctcf-wt.sh
@@ -44,4 +44,4 @@ do
 	echo $CELLTYPE1" mESC-CTCF-KO-rep1" >> ctcf_design.txt
 done
 
-python quantify_z.py 20 ctcf_design.txt 0.015
+python quantify_z.py 20 ctcf_design.txt 0.015 0.55 CTCF depletion
