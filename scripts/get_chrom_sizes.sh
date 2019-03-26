60270da31cf0222ca1f7fb00882697a75b40b4b1
Lila <lur159@psu.edu>
2019-03-26 09:34:28 -0400

    removed chr22

--- scripts/get_chrom_sizes.sh
+++ scripts/get_chrom_sizes.sh
@@ -1,11 +1,11 @@
 set -e
 
-if [ -e chrom_sizes.txt ]
+if [ -e output ]
 	then
 		rm output
 fi
 
-for CHROM in 22 21 20 19 18 17 16 15 14 13 12 11 10 8 7 6 5 4 3 2 1
+for CHROM in 21 20 19 18 17 16 15 14 13 12 11 10 8 7 6 5 4 3 2 1
 do
 	python get_chrom_size.py $CHROM >> output
 done
--- scripts/sup1.sh
+++ scripts/sup1.sh
@@ -5,7 +5,8 @@ RES=100000
 ./get_hic_data.sh GM12878_combined $RES
 ./get_hic_data.sh K562 $RES
 
-for METHOD in MultiMDS Independent_MDS
+#for METHOD in MultiMDS Independent_MDS
+for METHOD in Independent_MDS
 do
 	OUTPUT=$METHOD"_times".txt
 
@@ -14,13 +15,15 @@ do
 			rm $OUTPUT
 	fi
 
-	for CHROM in 22 21 20 19 18 17 16 15 14 13 12 11 10 8 7 6 5 4 3 2 1
+	for CHROM in 21 20 19 18 17 16 15 14 13 12 11 10 8 7 6 5 4 3 2 1
 	do
 		echo $CHROM
 		/usr/bin/time -o $OUTPUT -a -f %e python test_$METHOD.py $CHROM | awk '$1 == "rmsd" {print $2}' >> $OUTPUT
 	done
 
 done
+
+exit
 
 if [ ! -e chrom_sizes.txt ]
 	then
