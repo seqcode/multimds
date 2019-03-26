2d6af17d54dfb0398502a1ab61342b2a02ccc674
Lila <lur159@psu.edu>
2019-03-26 09:46:02 -0400

    fixed naming

--- scripts/sup4.py
+++ scripts/sup4.py
@@ -1,22 +1,15 @@
+import os
+import numpy as np
 import sys
 sys.path.append("..")
 import data_tools as dt
 import plotting as plot
-import os
 
-os.system("python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12_32kb.bed galactose_Scer_12_32kb.bed")
-struct1 = dt.structure_from_file("ctrl_Scer_12_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12_32kb_structure.tsv")
-plot.plot_structures_interactive((struct1, struct2), out_path="sup4a.png")
+os.system("python ../multimds.py -P 0.1 -w 0 ctrl_Scer_13_32kb.bed galactose_Scer_13_32kb.bed")
+struct1 = dt.structure_from_file("ctrl_Suva_13_32kb_structure.tsv")
+struct2 = dt.structure_from_file("galactose_Suva_13_32kb_structure.tsv")
 
-os.system("python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-upstream_32kb.bed galactose_Scer_12-upstream_32kb.bed")
-struct1 = dt.structure_from_file("ctrl_Scer_12-upstream_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12-upstream_32kb_structure.tsv")
+colors = np.zeros_like(struct1.getPoints(), dtype=int)
+colors[struct1.get_rel_index(852000)] = 1
 
-plot.plot_structures_interactive((struct1, struct2), out_path="sup4b_upstream.png")
-
-os.system("python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-downstream_32kb.bed galactose_Scer_12-downstream_32kb.bed")
-struct1 = dt.structure_from_file("ctrl_Scer_12-downstream_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12-downstream_32kb_structure.tsv")
-
-plot.plot_structures_interactive((struct1, struct2), out_path="sup4b_downstream.png")
+plot.plot_structures_interactive((struct1, struct2), (colors, colors))
