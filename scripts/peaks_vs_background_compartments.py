d542a9a070af83988f34d013d42b445a51c01c71
Lila <lur159@psu.edu>
2019-03-28 16:03:40 -0400

    reformatting

--- scripts/peaks_vs_background_compartments.py
+++ scripts/peaks_vs_background_compartments.py
@@ -16,9 +16,9 @@ ys = [background_comps[:,0] - background_comps[:,1], peak_comps[:,0] - peak_comp
 xs = range(len(ys))
 
 xmin = 0
-xmax = 0.6
+xmax = 0.4
 x_range = xmax - xmin
-x_start = xmin - x_range/5.	#larger offset for boxplot
+x_start = xmin - x_range/2.	#larger offset for boxplot
 x_end = xmax + x_range/10.
 
 ymin = min([min(y) for y in ys])
@@ -28,7 +28,7 @@ y_start = ymin - y_range/25.
 y_end = ymax + y_range/25.
 
 #plot data
-plt.boxplot(ys, positions=(0, 0.5), notch=True, patch_artist=True, labels=("Background", "Relocalized"), medianprops=dict(linestyle="none"))	#boxplot has built-in support for labels, unlike barplot
+plt.boxplot(ys, positions=(0, 0.3), notch=True, patch_artist=True, labels=("Background", "Relocalized"), medianprops=dict(linestyle="none"))	#boxplot has built-in support for labels, unlike barplot
 
 #define axes with offsets
 plt.axis([x_start, x_end, y_start, y_end], frameon=False)
@@ -38,7 +38,7 @@ plt.axvline(x=x_start, color="k", lw=4)
 plt.axhline(y=y_start, color="k", lw=4)
 
 #plot ticks
-plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=1, labelsize=8)
+plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=1, labelsize=9)
 
 plt.savefig("peaks_vs_background_change")
 plt.show()
@@ -54,7 +54,7 @@ ys = [background_comps[:,0], background_comps[:,1], peak_comps[:,0], peak_comps[
 xs = range(len(ys))
 
 xmin = 0
-xmax = 2
+xmax = 1.5
 x_range = xmax - xmin
 x_start = xmin - x_range/5.	#larger offset for boxplot
 x_end = xmax + x_range/10.
