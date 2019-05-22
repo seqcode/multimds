35b6462513380d7c59e03bd244e8e3e2b2bddc66
Lila <lur159@psu.edu>
2019-05-22 10:32:51 -0400

    fig1c scripts

--- scripts/fig1c.sh
+++ scripts/fig1c.sh
@@ -5,6 +5,5 @@ RES=100000
 ./get_hic_data.sh GM12878_combined $RES
 ./get_hic_data.sh K562 $RES
 
-python embedding_error.py
-
-mv GM12878_combined_K562_embedding_error.png fig1c.png
+python reproducibility.py GM12878_combined_21_100kb.bed K562_21_100kb.bed
+mv GM12878_combined_K562_reproducibility.png fig1c.png
