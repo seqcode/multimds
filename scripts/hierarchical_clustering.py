51eda19e7ecd0fa5a29ab7d2ed8ae211230b5a46
Lila <lur159@psu.edu>
2019-05-22 10:50:21 -0400

    add fig10 scripts

--- scripts/hierarchical_clustering.py
+++ scripts/hierarchical_clustering.py
@@ -8,5 +8,4 @@ os.system("paste peaks_filtered_{}_coverage.bed peaks_filtered_{}_coverage.bed p
 sns.set(font_scale=0.5)
 all_coverage = pd.read_table("peaks_filtered_all_coverage.bed", names=("GM12878 H3K27ac", "GM12878 H3K4me1", "GM12878 H3K4me3", "GM12878 H3K9ac", "GM12878 H2AZ", "GM12878 H3K4me2", "GM12878 H3K27me3", "K562 H3K27ac", "K562 H3K4me1", "K562 H3K4me3", "K562 H3K9ac", "K562 H2AZ", "K562 H3K4me2", "K562 H3K27me3"))
 sns.clustermap(all_coverage, cmap="Reds", col_cluster=False, cbar_kws={"label": "Coverage"})
-plt.savefig("hierarchical_clustering")
-plt.show()
+plt.savefig("fig10")
