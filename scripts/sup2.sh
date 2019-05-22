0d7c833d8a091fabd3c8e444ba8495f399405b5d
Lila <lur159@psu.edu>
2019-05-22 10:54:20 -0400

    add sup2 script

--- scripts/sup2.sh
+++ scripts/sup2.sh
@@ -1,26 +1,9 @@
 set -e
 
-./get_yeast_data.sh
+./process_ctcf-wt.sh
+./process_cohesin-wt.sh
 
-for STRAIN in Scer Suva
-do
-	python sup2.py $STRAIN 13 852000
-	python sup2.py $STRAIN 2 277000
-	python sup2.py $STRAIN 4 463434
-	python sup2.py $STRAIN 16 79711
-	
-	for CONDITION in ctrl galactose
-	do
-		cat hic_data/${CONDITION}_${STRAIN}_12_32kb.bed | awk '$2 >= 448000 && $5 >= 448000 {print $0}' > hic_data/${CONDITION}_${STRAIN}_12-downstream_32kb.bed
-	done
-
-	python sup2.py $STRAIN 12-downstream 448000
-done
-
-for CONDITION in ctrl galactose
-	do
-		cat hic_data/${CONDITION}_Scer_12_32kb.bed | awk '$2 < 448000 && $5 < 448000 {print $0}' > hic_data/${CONDITION}_Scer_12-upstream_32kb.bed
-done
-
-python sup2.py Scer 12-upstream 290212
-python sup2.py Suva 15 740086
+cd hic_data
+python ../reproducibility.py mESC-WT-rep1_19_100kb.bed hepatocyte-WT_19_100kb.bed
+cd ..
+mv hic_data/GM12878_combined_K562_reproducibility.png sup2.png
