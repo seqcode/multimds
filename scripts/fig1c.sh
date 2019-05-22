cb07f3f9e134d07fd536a608b6189e13c3127003
Lila <lur159@psu.edu>
2019-05-22 10:53:57 -0400

    paths

--- scripts/fig1c.sh
+++ scripts/fig1c.sh
@@ -6,6 +6,6 @@ do
 done
 
 cd hic_data
-python reproducibility.py GM12878_combined_21_100kb.bed K562_21_100kb.bed
+python ../reproducibility.py GM12878_combined_21_100kb.bed K562_21_100kb.bed
 cd ..
-mv GM12878_combined_K562_reproducibility.png fig1c.png
+mv hic_data/GM12878_combined_K562_reproducibility.png fig1c.png
