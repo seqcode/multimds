ee14d736bcc73968aa42e126b0a778facf8b9fc8
Lila <lur159@psu.edu>
2019-05-23 15:30:28 -0400

    rename fig3 scripts

--- scripts/fig3.sh
+++ scripts/fig3.sh
@@ -5,16 +5,16 @@ set -e
 for STRAIN in Scer Suva
 do
 	python ../multimds.py -P 0.1 -w 0 hic_data/ctrl_${STRAIN}_13_32kb.bed hic_data/galactose_${STRAIN}_13_32kb.bed
-	python plot_3d_yeast.py Has1-Tda1 13 852000 $STRAIN ""
+	python fig3.py Has1-Tda1 13 852000 $STRAIN
 
 	python ../multimds.py -P 0.1 -w 0 hic_data/ctrl_${STRAIN}_2_32kb.bed hic_data/galactose_${STRAIN}_2_32kb.bed
-	python plot_3d_yeast.py Gal1-7-10 2 277000 $STRAIN ""
+	python fig3.py Gal1-7-10 2 277000 $STRAIN
 
 	python ../multimds.py -P 0.1 -w 0 hic_data/ctrl_${STRAIN}_4_32kb.bed hic_data/galactose_${STRAIN}_4_32kb.bed 
-	python plot_3d_yeast.py Gal3 4 463434 $STRAIN ""
+	python fig3.py Gal3 4 463434 $STRAIN
 
 	python ../multimds.py -P 0.1 -w 0 hic_data/ctrl_${STRAIN}_16_32kb.bed hic_data/galactose_${STRAIN}_16_32kb.bed
-	python plot_3d_yeast.py Gal4 16 79711 $STRAIN ""
+	python fig3.py Gal4 16 79711 $STRAIN
 	
 	for CONDITION in ctrl galactose
 	do
@@ -22,7 +22,7 @@ do
 	done
 
 	python ../multimds.py -P 0.1 -w 0 hic_data/ctrl_${STRAIN}_12-downstream_32kb.bed hic_data/galactose_${STRAIN}_12-downstream_32kb.bed
-	python plot_3d_yeast.py rDNA 12-downstream 448000 $STRAIN ""
+	python fig3.py rDNA 12-downstream 448000 $STRAIN
 done
 
 for CONDITION in ctrl galactose
@@ -31,7 +31,7 @@ do
 done
 
 python ../multimds.py -P 0.1 -w 0 hic_data/ctrl_Scer_12-upstream_32kb.bed hic_data/galactose_Scer_12-upstream_32kb.bed
-python plot_3d_yeast.py Gal2 12-upstream 290212 Scer ""
+python fig3.py Gal2 12-upstream 290212 Scer
 
 python ../multimds.py -P 0.1 -w 0 hic_data/ctrl_Suva_15_32kb.bed hic_data/galactose_Suva_15_32kb.bed
-python plot_3d_yeast.py Gal2 15 740086 Suva ""
+python fig3.py Gal2 15 740086 Suva
