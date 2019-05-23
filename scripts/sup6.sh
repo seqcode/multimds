6115ee0f157e662416ead96ff9fa5c7b39adab06
Lila <lur159@psu.edu>
2019-05-23 13:34:33 -0400

    sup 6 scripts

--- scripts/sup6.sh
+++ scripts/sup6.sh
@@ -2,4 +2,7 @@ set -e
 
 ./get_yeast_data.sh
 
+python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12_32kb.bed galactose_Scer_12_32kb.bed
+python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-upstream_32kb.bed galactose_Scer_12-upstream_32kb.bed
+python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-downstream_32kb.bed galactose_Scer_12-downstream_32kb.bed
 python3.6 sup6.py
