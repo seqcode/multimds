4585dd082ba536d2f257c8945c5e21d601d08e62
Lila <lur159@psu.edu>
2019-05-23 13:32:49 -0400

    sup3 scripts

--- scripts/sup3.py
+++ scripts/sup3.py
@@ -2,21 +2,15 @@ import sys
 sys.path.append("..")
 import data_tools as dt
 import plotting as plot
-import os
 
-os.system("python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12_32kb.bed galactose_Scer_12_32kb.bed")
 struct1 = dt.structure_from_file("ctrl_Scer_12_32kb_structure.tsv")
 struct2 = dt.structure_from_file("galactose_Scer_12_32kb_structure.tsv")
-plot.plot_structures_interactive((struct1, struct2), out_path="sup4a.png")
+plot.plot_structures_interactive((struct1, struct2), out_path="sup3a.png")
 
-os.system("python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-upstream_32kb.bed galactose_Scer_12-upstream_32kb.bed")
 struct1 = dt.structure_from_file("ctrl_Scer_12-upstream_32kb_structure.tsv")
 struct2 = dt.structure_from_file("galactose_Scer_12-upstream_32kb_structure.tsv")
+plot.plot_structures_interactive((struct1, struct2), out_path="sup3b_upstream.png")
 
-plot.plot_structures_interactive((struct1, struct2), out_path="sup4b_upstream.png")
-
-os.system("python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-downstream_32kb.bed galactose_Scer_12-downstream_32kb.bed")
 struct1 = dt.structure_from_file("ctrl_Scer_12-downstream_32kb_structure.tsv")
 struct2 = dt.structure_from_file("galactose_Scer_12-downstream_32kb_structure.tsv")
-
-plot.plot_structures_interactive((struct1, struct2), out_path="sup4b_downstream.png")
+plot.plot_structures_interactive((struct1, struct2), out_path="sup3b_downstream.png")
--- scripts/sup3.sh
+++ scripts/sup3.sh
@@ -1,32 +1,9 @@
 set -e
 
-RES=100000
+./get_yeast_data.sh
 
-./get_hic_data.sh GM12878_combined $RES
-./get_hic_data.sh K562 $RES
+python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12_32kb.bed galactose_Scer_12_32kb.bed
+python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-upstream_32kb.bed galactose_Scer_12-upstream_32kb.bed
+python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-downstream_32kb.bed galactose_Scer_12-downstream_32kb.bed
 
-for METHOD in MultiMDS Independent_MDS
-do
-	OUTPUT=$METHOD"_times".txt
-
-	if [ -e $OUTPUT ]
-		then
-			rm $OUTPUT
-	fi
-
-	for CHROM in 21 20 19 18 17 16 15 14 13 12 11 10 8 7 6 5 4 3 2 1
-	do
-		echo $CHROM
-		/usr/bin/time -o $OUTPUT -a -f %e python test_$METHOD.py $CHROM | awk '$1 == "rmsd" {print $2}' >> $OUTPUT
-	done
-
-done
-
-exit
-
-if [ ! -e chrom_sizes.txt ]
-	then
-		./get_chrom_sizes.sh
-fi
-
-python time_methods.py
+python sup3.py
