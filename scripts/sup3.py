82878a37ae1d0a303c6acde97518fee607a29243
Lila <lur159@psu.edu>
2019-05-23 15:38:40 -0400

    rename sup3 scripts

--- scripts/sup3.py
+++ scripts/sup3.py
@@ -1,16 +1,28 @@
-import sys
-sys.path.append("..")
-import data_tools as dt
-import plotting as plot
+from matplotlib import pyplot as plt
+import numpy as np
 
-struct1 = dt.structure_from_file("ctrl_Scer_12_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12_32kb_structure.tsv")
-plot.plot_structures_interactive((struct1, struct2), out_path="sup3a.png")
+chrom_sizes = np.loadtxt("chrom_sizes.txt")
 
-struct1 = dt.structure_from_file("ctrl_Scer_12-upstream_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12-upstream_32kb_structure.tsv")
-plot.plot_structures_interactive((struct1, struct2), out_path="sup3b_upstream.png")
+plt.subplot2grid((10,10), (0,0), 9, 10, frameon=False)
+maxs = []
+for method in ("MultiMDS", "Independent_MDS"):
+	times = np.loadtxt("{}_times.txt".format(method))/60.
+	plt.scatter(chrom_sizes, times, label=" ".join(method.split("_")))
+	maxs.append(max(times))
 
-struct1 = dt.structure_from_file("ctrl_Scer_12-downstream_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12-downstream_32kb_structure.tsv")
-plot.plot_structures_interactive((struct1, struct2), out_path="sup3b_downstream.png")
+xs = chrom_sizes
+x_int_size = 500
+y_int_size = 1
+x_start = min(xs) - x_int_size/4.
+x_end = max(xs) + x_int_size/5.
+y_start = -y_int_size/5.
+y_end = max(maxs) + y_int_size/5.
+
+plt.xlabel("Number of bins", fontsize=14)
+plt.ylabel("Computational time (minutes)", fontsize=14)
+plt.axis([x_start, x_end, y_start, y_end], frameon=False)
+plt.axvline(x=x_start, color="k", lw=4)
+plt.axhline(y=y_start, color="k", lw=6)
+plt.legend()
+plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=5, labelsize=10)
+plt.savefig("sup3")
--- scripts/sup3.sh
+++ scripts/sup3.sh
@@ -1,9 +1,32 @@
 set -e
 
-./get_yeast_data.sh
+RES=100000
 
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12_32kb.bed galactose_Scer_12_32kb.bed
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-upstream_32kb.bed galactose_Scer_12-upstream_32kb.bed
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-downstream_32kb.bed galactose_Scer_12-downstream_32kb.bed
+./get_hic_data.sh GM12878_combined $RES
+./get_hic_data.sh K562 $RES
+
+for METHOD in MultiMDS Independent_MDS
+do
+	OUTPUT=$METHOD"_times".txt
+
+	if [ -e $OUTPUT ]
+		then
+			rm $OUTPUT
+	fi
+
+	for CHROM in 21 20 19 18 17 16 15 14 13 12 11 10 8 7 6 5 4 3 2 1
+	do
+		echo $CHROM
+		/usr/bin/time -o $OUTPUT -a -f %e python test_$METHOD.py $CHROM | awk '$1 == "rmsd" {print $2}' >> $OUTPUT
+	done
+
+done
+
+exit
+
+if [ ! -e chrom_sizes.txt ]
+	then
+		./get_chrom_sizes.sh
+fi
 
 python sup3.py
