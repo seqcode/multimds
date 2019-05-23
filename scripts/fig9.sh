2bab33d842a87abb504177e7838079c0501ebe70
Lila <lur159@psu.edu>
2019-05-23 14:50:19 -0400

    reorganize negative control

--- scripts/fig9.sh
+++ scripts/fig9.sh
@@ -9,22 +9,6 @@ RES_KB=$(($RES/1000))
 ./relocalization_peaks.sh $RES
 ./get_state_data.sh
 
-#negative control
-if [ ! -e A_compartment_${RES_KB}kb.bed ]
-	then
-		python get_a_compartment.py $RES
-fi
-
-if [ ! -e A_background.bed ]
-	then
-		bedtools subtract -a A_compartment_${RES_KB}kb.bed -b peaks_filtered.bed > A_background.bed
-fi
-
-if [ ! -e A_background_filtered.bed ]
-	then
-		./filter_mappability.sh A_background $RES
-fi
-
 IDS=(H3k27acStdPk H3k04me1StdPkV2 H3k04me3StdPkV2 H3k9acStdPk H3k36me3StdPk H2azStdPk H3k4me2StdPk H3k79me2StdPk H4k20me1StdPk H3k27me3StdPkV2 Ezh239875Pk H3k9me3StdPk H3k36me3StdPk CtcfStdPk)
 NAMES=(H3K27ac H3K4me1 H3K4me3 H3K9ac H3K36me3 H2AZ H3K4me2 H3K79me2 H4K20me1 H3K27me3 EZH2 H3K9me3 H3K36me3 CTCF)
 
--- scripts/relocalization_peaks_independent.sh
+++ scripts/relocalization_peaks_independent.sh
@@ -16,3 +16,19 @@ do
 	./filter_mappability.sh ${CHROM}_A_noncomp_peaks_independent $RES
 	cat ${CHROM}_A_noncomp_peaks_independent_filtered.bed >> peaks_filtered_independent.bed
 done
+
+#negative control
+if [ ! -e A_compartment_${RES_KB}kb.bed ]
+	then
+		python get_a_compartment.py $RES
+fi
+
+if [ ! -e A_background.bed ]
+	then
+		bedtools subtract -a A_compartment_${RES_KB}kb.bed -b peaks_filtered.bed > A_background.bed
+fi
+
+if [ ! -e A_background_filtered.bed ]
+	then
+		./filter_mappability.sh A_background $RES
+fi
