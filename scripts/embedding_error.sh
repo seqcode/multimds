8fcc406e5f42ac7f57a338bd25964a2614f821db
Lila <lur159@psu.edu>
2019-05-23 16:19:47 -0400

    removed unused scripts

--- scripts/embedding_error.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-./get_hic_data.sh GM12878_combined
-./get_hic_data.sh K562
-
-python embedding_error.py 21
--- scripts/quantify_z_cohesin.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-#./process_cohesin.sh
-echo "hepatocyte-cohesin-KO hepatocyte-WT" > cohesin_design.txt
-python quantify_z.py 20 cohesin_design.txt 0.04
