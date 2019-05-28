cf1ed3b803b23cbdcfe291c9f93c5865926067eb
Lila <lur159@psu.edu>
2019-05-28 08:42:19 -0400

    fig1c

--- scripts/fig1c.sh
+++ scripts/fig1c.sh
@@ -5,7 +5,5 @@ do
 	./get_hic_data.sh $CELLTYPE 100000
 done
 
-cd hic_data
-python ../reproducibility.py GM12878_combined_21_100kb.bed K562_21_100kb.bed
-cd ..
-mv hic_data/GM12878_combined_K562_reproducibility.png fig1c.png
+python reproducibility.py hic_data/GM12878_combined_21_100kb.bed hic_data/K562_21_100kb.bed
+mv GM12878_combined_21_100kb_K562_21_100kb_reproducibility.png fig1c.png
