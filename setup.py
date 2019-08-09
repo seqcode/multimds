217f04df2fb5c758ce4d03bd73f585ff243fc008
Lila <lur159@psu.edu>
2019-08-09 14:25:41 -0400

    for pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="multimds",
+    version="0.0.1",
+    author="Lila Rieber",
+    author_email="lr65358@example.com",
+    description="Structural inference and alignment of Hi-C datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seqcode/multimds",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
