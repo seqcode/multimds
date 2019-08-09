2414376c52136950ea710ff0f6a40411f03d98c2
Lila <lur159@psu.edu>
2019-08-09 11:18:42 -0400

    add init

--- multimds/__init__.py
+++ multimds/__init__.py
@@ -1,1 +1,5 @@
-from .array_tools import contactToDist
+from .multimds import partitioned_mds, full_mds
+from .compartment_fraction import calculate_compartment_fraction
+from .linear_algebra import calculate_distances
+from .data_tools import structure_from_file
+from .plotting import plot_structure_interactive, plot_structures_interactive, plot_structure_gif, plot_structures_gif
