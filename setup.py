3848021d53ed9102f1682cdaef719b8d42c0bcfb
Lila <lur159@psu.edu>
2019-08-14 14:35:55 -0400

    pip build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="multimds",
-    version="0.0.2",
+    version="0.0.3",
     author="Lila Rieber",
     author_email="lr65358@gmail.com",
     description="Structural inference and alignment of Hi-C datasets",
