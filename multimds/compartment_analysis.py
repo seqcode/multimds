8aac0afe5d2b7cba1724a1af94bc13a7893cd312
Lila <lur159@psu.edu>
2019-08-14 10:14:57 -0400

    remove redundant method

--- multimds/compartment_analysis.py
+++ multimds/compartment_analysis.py
@@ -4,40 +4,7 @@ from scipy import stats as st
 from .data_tools import *
 from sklearn import svm
 from .linear_algebra import *
-
-def oe(mat):
-	n = len(mat)
-
-	tots = np.zeros(n-1)
-	counts = np.zeros(n-1)
-	
-	for i in range(n):
-		for j in range(i):
-			observed = mat[i,j]
-			if observed != 0:
-				s = i - j
-				tots[s - 1] += observed
-				counts[s - 1] += 1
-
-	avgs = np.zeros(n-1)
-
-	for i, count in enumerate(counts):
-		if count != 0:
-			avgs[i] = tots[i]/count
-
-	oe_mat = np.zeros_like(mat)
-
-	for i in range(n):
-		for j in range(i):
-			observed = mat[i,j]
-			s = i-j
-			expected = avgs[s-1]
-			if expected != 0:
-				ratio = observed/expected	
-				oe_mat[i,j] = ratio
-				oe_mat[j,i] = ratio
-
-	return oe_mat
+from .hic_oe import oe
 
 def cor(mat):
 	"""Correlation of rows with columns of mat"""
