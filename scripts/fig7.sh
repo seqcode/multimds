c608c646efed70fbb33abd2c0869d6588295f8a3
Lila <lur159@psu.edu>
2019-08-20 16:30:13 -0400

    update method calling

--- scripts/fig7.sh
+++ scripts/fig7.sh
@@ -8,14 +8,18 @@ do
 	./get_hic_data.sh $CELLTYPE $RES
 done
 
-python ../multimds.py hic_data/IMR90_21_${RES_KB}kb.bed hic_data/HMEC_21_${RES_KB}kb.bed
+#python ../multimds.py hic_data/IMR90_21_${RES_KB}kb.bed hic_data/HMEC_21_${RES_KB}kb.bed
+python fig6_multimds.py IMR90 HMEC
 python fig7.py IMR90 $RES_KB
 
-python ../multimds.py hic_data/HMEC_21_${RES_KB}kb.bed hic_data/HUVEC_21_${RES_KB}kb.bed
+#python ../multimds.py hic_data/HMEC_21_${RES_KB}kb.bed hic_data/HUVEC_21_${RES_KB}kb.bed
+python fig6_multimds.py IMR90 HUVEC
 python fig7.py HMEC $RES_KB
 
-python ../multimds.py hic_data/IMR90_21_${RES_KB}kb.bed hic_data/HUVEC_21_${RES_KB}kb.bed
+#python ../multimds.py hic_data/IMR90_21_${RES_KB}kb.bed hic_data/HUVEC_21_${RES_KB}kb.bed
+python fig6_multimds.py HMEC HUVEC
 python fig7.py HUVEC $RES_KB
 
-python ../multimds.py hic_data/K562_21_${RES_KB}kb.bed hic_data/HUVEC_21_${RES_KB}kb.bed
+#python ../multimds.py hic_data/K562_21_${RES_KB}kb.bed hic_data/HUVEC_21_${RES_KB}kb.bed
+python fig6_multimds.py K562 HUVEC
 python fig7.py K562 $RES_KB
