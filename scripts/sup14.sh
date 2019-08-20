6a1874e51188d352dcf4e6f90a6a14f5ac6d7bf7
Lila <lur159@psu.edu>
2019-08-20 10:09:27 -0400

    reorganize

--- scripts/sup14.sh
+++ scripts/sup14.sh
@@ -1,10 +1,8 @@
 set -e
 
-for CELLTYPE in GM12878_primary GM12878_replicate
+for CELLTYPE in GM12878_combined K562
 do
 	./get_hic_data.sh $CELLTYPE 100000
 done
 
-echo "GM12878_primary GM12878_replicate" > encode_rep_design.txt
-
-python quantify_z.py 23 encode_rep_design.txt 0.025 0.5 GM12878 reps
+python sup14.py
