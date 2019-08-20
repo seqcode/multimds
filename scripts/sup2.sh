60169ce2978e98eee128aa0589e78ec2845e3456
Lila <lur159@psu.edu>
2019-08-20 10:40:42 -0400

    add sup2 scripts

--- scripts/sup2.sh
+++ scripts/sup2.sh
@@ -1,7 +1,8 @@
 set -e
 
-./process_ctcf-wt.sh
-./process_cohesin-wt.sh
+for CELLTYPE in GM12878_combined K562
+do
+	./get_hic_data.sh $CELLTYPE 100000
+done
 
-python reproducibility.py hic_data/mESC-WT-rep1_19_100kb.bed hic_data/hepatocyte-WT_19_100kb.bed
-mv mESC-WT-rep1_19_100kb_hepatocyte-WT_19_100kb_reproducibility.png sup2.png
+python sup1.py
