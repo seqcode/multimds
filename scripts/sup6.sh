3fccc32974967e8b142ae236d427a79c4f22dce5
Lila <lur159@psu.edu>
2019-08-20 13:32:15 -0400

    add sup6 script

--- scripts/sup6.sh
+++ scripts/sup6.sh
@@ -1,8 +1,6 @@
 set -e
 
-./get_yeast_data.sh
-
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12_32kb.bed galactose_Scer_12_32kb.bed
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-upstream_32kb.bed galactose_Scer_12-upstream_32kb.bed
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-downstream_32kb.bed galactose_Scer_12-downstream_32kb.bed
-python sup6.py
+./simulate_tads.sh
+	
+python make_heatmap.py 1
+python make_heatmap.py 2
