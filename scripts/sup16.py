12c9754dfbab67dba8d2941430b16a6388181ebe
Lila <lur159@psu.edu>
2019-08-20 10:00:49 -0400

    reorganize

--- scripts/sup16.py
+++ scripts/sup16.py
@@ -1,76 +1,84 @@
 import sys
 sys.path.append("..")
-import compartment_analysis as ca
 import data_tools as dt
-from scipy import stats as st
-from matplotlib import pyplot as plt
 import numpy as np
-import linear_algebra as la
-from scipy import signal as sg
+from matplotlib import pyplot as plt
+from scipy import stats as st
+import compartment_analysis as ca
 import os
 
-chrom = 19
-res_kb = 100
+comparisons = ("mouse_celltype", "encode", "cohesin", "lymphoblastoid", "mouse_celltype_rep", "encode_rep")
+boxes = [[] for comparison in comparisons]
 
-os.system("python ../multimds.py -o test_ hic_data/GM12878_combined_{}_{}kb.bed hic_data/K562_{}_{}kb.bed".format(chrom, res_kb, chrom, res_kb))
+for i, comparison in enumerate(comparisons):
+	with open("{}_design.txt".format(comparison)) as infile:
+		for line in infile:
+			prefix1, prefix2 = line.strip().split()
+			for chrom in range(1, 23):
+				path1 = "hic_data/{}_{}_100kb.bed".format(prefix1, chrom)
+				path2 = "hic_data/{}_{}_100kb.bed".format(prefix2, chrom)
 
-struct1 = dt.structure_from_file("test_GM12878_combined_{}_{}kb_structure.tsv".format(chrom, res_kb))	
-struct2 = dt.structure_from_file("test_K562_{}_{}kb_structure.tsv".format(chrom, res_kb))
+				if os.path.isfile(path1) and os.path.isfile(path2):
 
-mat1 = dt.matFromBed("hic_data/GM12878_combined_{}_{}kb.bed".format(chrom, res_kb), struct1)
-comps1 = ca.get_compartments(mat1)
-mat2 = dt.matFromBed("hic_data/K562_{}_{}kb.bed".format(chrom, res_kb), struct2)
-comps2 = ca.get_compartments(mat2)
+					#load structures
+					structure1 = dt.structureFromBed(path1)	
+					structure2 = dt.structureFromBed(path2)
 
-r, p = st.pearsonr(comps1, comps2)
-if r < 0:
-	comps1 = -comps1
+					dt.make_compatible((structure1, structure2))
+	
+					mat1 = dt.matFromBed(path1, structure1)
+					mat2 = dt.matFromBed(path2, structure2)
 
-comp_diffs = np.abs(comps1 - comps2)
+					comps1 = ca.get_compartments(mat1)
+					comps2 = ca.get_compartments(mat2)
 
-dists = np.array([la.calcDistance(coord1, coord2) for coord1, coord2 in zip(struct1.getCoords(), struct2.getCoords())])
-dist_peaks = sg.find_peaks_cwt(dists, np.arange(1,10))
+					r, p = st.pearsonr(comps1, comps2)
 
+					boxes[i].append(np.abs(r))
+
+		infile.close()
+
+#start with a frameless plot (extra room on the left)
 plt.subplot2grid((10,10), (0,0), 9, 10, frameon=False)
-gen_coords = struct1.getGenCoords()
-plt.plot(gen_coords, comp_diffs/max(comp_diffs), lw=2, color=(0.75,0,0), label="Compartment score change", zorder=1)
-plt.plot(gen_coords, dists/max(dists), lw=2, color=(0,0,0.75), label="Relocalization", zorder=1)
 
-for dist_peak in dist_peaks:
-	if comp_diffs[dist_peak] < 0.2:
-		plt.scatter([gen_coords[dist_peak]], [0.1], color=(0,0,1), s=40, zorder=2)
-	else:
-		plt.scatter([gen_coords[dist_peak]], [0.1], color=(0.25,0.25,0.25), s=40, zorder=2)
-
-
-plt.xlabel("Genomic coordinate", fontsize=20)
-plt.ylabel("Normalized change", fontsize=20)
+#label axes
+plt.ylabel("Compartment correlation", fontsize=10)
 
 #define offsets
-xmin = min(gen_coords)
-xmax = max(gen_coords)
-x_range = xmax - xmin
-x_start = xmin - x_range/25.
-x_end = xmax + x_range/25.
+ys = boxes
+n = len(ys)
+width = 0.075
 
-ymin = 0
-ymax = 1
+xmin = 0	#boxplot indexing starts at 1
+xmax = n*width*2
+x_range = xmax - xmin
+x_start = xmin - x_range/10.	#larger offset for boxplot
+x_end = xmax + x_range/10.
+
+ymin = min([min(y) for y in ys])
+ymax = max([max(y) for y in ys])
 y_range = ymax - ymin
 y_start = ymin - y_range/25.
 y_end = ymax + y_range/25.
+
+#plot data
+#labels = ["Mouse cell types"]
+
+#plt.boxplot(ys, labels=("Mouse cell types", "ENCODE", "Cohesin KO", "LCLs", "Mouse cell type reps", "GM12878 reps"))
+#plt.show()
+
+plt.boxplot(ys, notch=True, patch_artist=True, positions=np.arange(width, n*(width*2), width*2), widths=[width for i in range(n)], labels=("Mouse cell types", "ENCODE", "Cohesin KO", "LCLs", "Mouse cell type reps", "GM12878 reps"), medianprops=dict(linestyle="none"))	#boxplot has built-in support for labels, unlike barplot
+#plt.boxplot(ys, notch=True, patch_artist=True, positions=np.arange(width, n*(width*2), width*2), widths=[0.1 for i in range(n)], labels=labels, medianprops=dict(linestyle="none"))
 
 #define axes with offsets
 plt.axis([x_start, x_end, y_start, y_end], frameon=False)
 
 #plot axes (black with line width of 4)
-plt.axvline(x=x_start, color="k", lw=2)
+plt.axvline(x=x_start, color="k", lw=4)
 plt.axhline(y=y_start, color="k", lw=4)
 
 #plot ticks
-plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=1, labelsize=18)
-
-plt.plot([x_start, x_end], [0.2, 0.2], color=(0.5,0.5,0.5), linestyle="--")
-
-plt.legend(frameon=False, loc=2, fontsize=16)
+plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=5, labelsize=12)
 
 plt.savefig("sup16")
+plt.show()
