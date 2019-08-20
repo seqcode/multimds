92d6f01501d100d0f0aa04ffad827bd53d44f379
Lila <lur159@psu.edu>
2019-08-20 10:15:15 -0400

    reorganize

--- scripts/sup12.sh
+++ scripts/sup12.sh
@@ -1,95 +1,96 @@
 set -e
 
-#ENCODE
-for CELLTYPE in GM12878_primary GM12878_replicate K562 KBM7 IMR90 HUVEC HMEC NHEK
-do
-	./get_hic_data.sh $CELLTYPE 100000
-done
-
-if [ -e encode_design.txt ]
+if [ ! -e GSM2583734_galactose_IP.bedgraph ]
 	then
-		rm encode_design.txt
+		curl ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM2583nnn/GSM2583734/suppl/GSM2583734_galactose_IP.bedgraph.gz -o GSM2583734_galactose_IP.bedgraph.gz
+		gunzip GSM2583734_galactose_IP.bedgraph.gz
 fi
 
-for CELLTYPE1 in GM12878_primary GM12878_replicate
-do
-	for CELLTYPE2 in K562 IMR90 HUVEC HMEC NHEK
-	do
-		echo $CELLTYPE1" "$CELLTYPE2 >> encode_design.txt
-	done
-done
-
-CELLTYPES=(K562 IMR90 HUVEC HMEC NHEK)
-
-for i in `seq 0 $((${#CELLTYPES[@]}-1))`
-do	
-	for j in `seq 0 $(($i-1))`
-	do
-		echo ${CELLTYPES[$i]}" "${CELLTYPES[$j]} >> encode_design.txt
-	done
-done
-
-python quantify_z_independent.py 23 encode_design.txt 0.025
-
-#mouse cell types
-RES=100000
-
-./process_hpc7.sh $RES
-./process_g1e.sh WT $RES
-./process_cohesin-wt.sh
-./process_ctcf-wt.sh
-
-if [ -e mouse_celltype_design.txt ]
+if [ ! -e GSM2583735_galactose_input.bedgraph ]
 	then
-		rm mouse_celltype_design.txt
+		curl ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM2583nnn/GSM2583735/suppl/GSM2583735_galactose_input.bedgraph.gz -o GSM2583735_galactose_input.bedgraph.gz
+		gunzip GSM2583735_galactose_input.bedgraph.gz
 fi
 
-CELLTYPES=(mESC-WT-rep1 mESC-WT-rep2 HPC7-rep1 HPC7-rep2 WT-G1E hepatocyte-WT)
-
-for CELLTYPE1 in mESC-WT-rep1 mESC-WT-rep2
-do
-	for CELLTYPE2 in HPC7-rep1 HPC7-rep2 WT-G1E hepatocyte-WT
-	do
-		echo $CELLTYPE1" "$CELLTYPE2 >> mouse_celltype_design.txt
-	done
-done
-
-for CELLTYPE1 in HPC7-rep1 HPC7-rep2
-do
-	for CELLTYPE2 in mESC-WT-rep1 mESC-WT-rep2 WT-G1E hepatocyte-WT
-	do
-		echo $CELLTYPE1" "$CELLTYPE2 >> mouse_celltype_design.txt
-	done
-done
-
-echo "WT-G1E hepatocyte-WT" >> mouse_celltype_design.txt
-
-python quantify_z_independent.py 20 mouse_celltype_design.txt 0.035
-
-#LCL
-./get_lymphoblastoid.sh
-
-if [ -e lymphoblastoid_design.txt ]
+if [ ! -e GSM2583736_glucose_IP.bedgraph ]
 	then
-		rm lymphoblastoid_design.txt
+		curl ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM2583nnn/GSM2583736/suppl/GSM2583736_glucose_IP.bedgraph.gz -o GSM2583736_glucose_IP.bedgraph.gz
+		gunzip GSM2583736_glucose_IP.bedgraph.gz
 fi
 
-CELLTYPES=(GM19238 GM19239 GM19240 HG00512 HG00513 HG00514 HG00731 HG00732 HG00733)
+if [ ! -e GSM2583737_glucose_input.bedgraph ]
+	then
+		curl ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM2583nnn/GSM2583737/suppl/GSM2583737_glucose_input.bedgraph.gz -o GSM2583737_glucose_input.bedgraph.gz
+		gunzip GSM2583737_glucose_input.bedgraph.gz
+fi
 
-for i in `seq 0 $((${#CELLTYPES[@]}-1))`
+#align
+for NUM in `seq 3 6`
 do
-	CELLTYPE1=${CELLTYPES[$i]}
-	for j in `seq 0 $(($i-1))`
-	do
-		CELLTYPE2=${CELLTYPES[$j]}
-		echo $CELLTYPE1" "$CELLTYPE2 >> lymphoblastoid_design.txt
-	done
+	SRR=545748$NUM
+ 
+	if [ ! -e SRR${SRR}.sam ]
+		then
 
+			#download
+			curl ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR545/SRR$SRR/SRR$SRR.sra -o SRR$SRR.sra
+
+			#fastq-dump
+			fastq-dump --split-files SRR$SRR.sra
+			rm SRR$SRR.sra
+
+		#index
+		for READ in 1 2
+		do
+			bwa aln /data/drive1/genomes/Sc_Su/Sc_Su.fa SRR${SRR}_$READ.fastq > SRR${SRR}_$READ.sai
+		done
+
+		#map
+		bwa sampe /data/drive1/genomes/Sc_Su/Sc_Su.fa SRR${SRR}_1.sai SRR${SRR}_2.sai SRR${SRR}_1.fastq SRR${SRR}_2.fastq > SRR${SRR}.sam
+	fi
 done
 
-python quantify_z_independent.py 20 lymphoblastoid_design.txt 0.03
+#call peaks independently
+if [ ! -e galactose_Nup60_peaks.broadPeak ]
+	then
+		macs2 callpeak --broad -t SRR5457483.sam -c SRR5457484.sam -n galactose_Nup60 --nomodel
+fi
+if [ ! -e glucose_Nup60_peaks.broadPeak ]
+	then
+		macs2 callpeak --broad -t SRR5457485.sam -c SRR5457486.sam -n glucose_Nup60 --nomodel
+fi
 
-#cohesin
-./process_cohesin-ko.sh
-echo "hepatocyte-cohesin-KO hepatocyte-WT" > cohesin_design.txt
-python quantify_z_independent.py 20 cohesin_design.txt 0.04
+#merge
+cp galactose_Nup60_peaks.broadPeak all_Nup60.bed
+cat glucose_Nup60_peaks.broadPeak >> all_Nup60.bed
+
+bedtools sort -i all_Nup60.bed > all_Nup60_sorted.bed
+bedtools merge -i all_Nup60_sorted.bed > all_Nup60_merged.bed
+
+#convert to bam
+for NUM in 3 5
+do
+	if [ ! -e SRR545748$NUM.bam ]
+		then
+			samtools view -b SRR545748$NUM.sam > SRR545748$NUM.bam
+	fi
+done
+
+#get tag counts
+bedtools coverage -a all_Nup60_merged.bed -b SRR5457483.bam -counts > galactose_coverage.bed
+bedtools coverage -a all_Nup60_merged.bed -b SRR5457485.bam -counts > glucose_coverage.bed
+
+echo "Symbol	ctrl	galactose" > nup60_counts.tsv
+paste glucose_coverage.bed galactose_coverage.bed | cut -d "	" -f 1,2,3,4,8 | awk '{print $1":"$2"-"$3"\t"$4"\t"$5}' >> nup60_counts.tsv
+
+#get differential peaks
+/usr/bin/Rscript run_edger.R
+python process_edger_results.py
+
+for GENE in Gal1-7-10 Gal2 Has1-Tda1 Gal3 Gal4 Hxt1
+do
+	bedtools intersect -a nup60_sig.bed -b ${GENE}.bed -f 0.5 > ${GENE}_Nup60_peak.bed
+	bedtools intersect -a ctrl_IP.bedgraph -b ${GENE}.bed > ctrl_${GENE}_Nup60.bed
+	bedtools intersect -a galactose_IP.bedgraph -b ${GENE}.bed > galactose_${GENE}_Nup60.bed
+	python sup12.py $GENE
+done
