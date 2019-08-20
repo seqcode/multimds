b76206d69c8d91b03c052dba91150c3e24f00368
Lila <lur159@psu.edu>
2019-08-20 14:19:33 -0400

    add sup7 script

--- scripts/sup7.py
+++ scripts/sup7.py
@@ -1,48 +1,48 @@
 import sys
-sys.path.append("..")
-import os
+sys.path.append("../..")
 import data_tools as dt
-import compartment_analysis as ca
-import numpy as np
-from sklearn import svm
+from matplotlib import pyplot as plt
 import linear_algebra as la
-from mayavi import mlab
 
-path = "hic_data/GM12878_combined_21_100kb.bed"
-os.system("python ../minimds.py {}".format(path))
-struct = dt.structure_from_file("GM12878_combined_21_100kb_structure.tsv")
+struct1 = dt.structure_from_file("sim1_chr21_100kb_structure.tsv")
+struct2 = dt.structure_from_file("sim2_chr21_100kb_structure.tsv")
 
-new_start = struct.chrom.getAbsoluteIndex(15000000)
-struct.subsamplePoints(new_start, len(struct.points)-3)
+#dt.make_compatible((struct1, struct2))
+#struct1.rescale()
+#struct2.rescale()
+#r, t = la.getTransformation(struct1, struct2)
+#struct1.transform(r,t)
 
-#compartments
-contacts = dt.matFromBed(path, struct)
+gen_coords = struct1.getGenCoords()
+dists = [la.calcDistance(coord1, coord2) for coord1, coord2 in zip(struct1.getCoords(), struct2.getCoords())]
 
-compartments = np.array(ca.get_compartments(contacts, 1))
+plt.subplot2grid((10,10), (0,0), 9, 10, frameon=False)
+plt.plot(gen_coords, dists, lw=2)
 
-#SVR
-coords = struct.getCoords()
-clf = svm.LinearSVR()
-clf.fit(coords, compartments)
-coef = clf.coef_
+#define offsets
+xmin = min(gen_coords)
+xmax = max(gen_coords)
+x_range = xmax - xmin
+x_start = xmin - x_range/25.
+x_end = xmax + x_range/25.
 
-transformed_coords = np.array(la.change_coordinate_system(coef, coords))
-xs = transformed_coords[:,0]
-min_x = min(xs)
-max_x = max(xs)
-x_range = max_x - min_x
-ys = transformed_coords[:,1]
-min_y = min(ys)
-max_y = max(ys)
-y_range = max_y - min_y
-zs = transformed_coords[:,2]
-min_z = min(zs)
-max_z = max(zs)
+ymin = 0
+ymax = max(dists)
+y_range = ymax - ymin
+y_start = ymin - y_range/25.
+y_end = ymax + y_range/25.
 
-mlab.figure(bgcolor=(1,1,1))
-mlab.plot3d(xs, ys, zs, compartments, colormap="bwr")
-x_coord = max_x + x_range/10
-y_coord = max_y + y_range/10
-mlab.quiver3d([0], [0], [1], extent=[0, 0, 0, 0, min_z, max_z], color=(0,0,0), line_width=8)
-mlab.savefig("compartments_3D.png")
-mlab.show()
+#define axes with offsets
+plt.axis([x_start, x_end, y_start, y_end], frameon=False)
+
+#plot axes (black with line width of 4)
+plt.axvline(x=x_start, color="k", lw=4)
+plt.axhline(y=y_start, color="k", lw=4)
+
+plt.xlabel("Genomic coordinate", fontsize=12)
+plt.ylabel("Relocalization distance", fontsize=12)
+
+#plot ticks
+plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=1, labelsize=10)
+
+plt.show()
