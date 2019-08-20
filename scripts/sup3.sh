81fa1ab74cd085edb99b974b94dc9463f8df60d5
Lila <lur159@psu.edu>
2019-08-20 10:24:51 -0400

    reorganize

--- scripts/sup3.sh
+++ scripts/sup3.sh
@@ -1,32 +1,7 @@
 set -e
 
-RES=100000
+./process_ctcf-wt.sh
+./process_cohesin-wt.sh
 
-./get_hic_data.sh GM12878_combined $RES
-./get_hic_data.sh K562 $RES
-
-for METHOD in MultiMDS Independent_MDS
-do
-	OUTPUT=$METHOD"_times".txt
-
-	if [ -e $OUTPUT ]
-		then
-			rm $OUTPUT
-	fi
-
-	for CHROM in 21 20 19 18 17 16 15 14 13 12 11 10 8 7 6 5 4 3 2 1
-	do
-		echo $CHROM
-		/usr/bin/time -o $OUTPUT -a -f %e python test_$METHOD.py $CHROM | awk '$1 == "rmsd" {print $2}' >> $OUTPUT
-	done
-
-done
-
-exit
-
-if [ ! -e chrom_sizes.txt ]
-	then
-		./get_chrom_sizes.sh
-fi
-
-python sup3.py
+python reproducibility.py hic_data/mESC-WT-rep1_19_100kb.bed hic_data/hepatocyte-WT_19_100kb.bed
+mv mESC-WT-rep1_19_100kb_hepatocyte-WT_19_100kb_reproducibility.png sup3.png
