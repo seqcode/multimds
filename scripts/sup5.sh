0f08651d40813743ac8b51f894d4bc4ee1f45308
Lila <lur159@psu.edu>
2019-08-21 17:00:35 -0400

    update method calling

--- scripts/sup5.sh
+++ scripts/sup5.sh
@@ -5,8 +5,6 @@ do
 	./get_hic_data.sh $CELLTYPE 5000
 done
 
-python ../multimds.py GM12878_combined_21_5kb.bed K562_21_5kb.bed
-
 if [ ! -e mappability_21_5kb.bed ]
 	then
 		cat mappability_5kb.bed | awk '$1 == "chr21" {print $0}' > mappability_21_5kb.bed
