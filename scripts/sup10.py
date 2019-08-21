a34c5fed6fa12411ec1c3c47668ae634341b5d57
Lila <lur159@psu.edu>
2019-08-21 17:39:56 -0400

    update method calling

--- scripts/sup10.py
+++ scripts/sup10.py
@@ -1,16 +1,11 @@
-import sys
-sys.path.append("..")
-import data_tools as dt
-import plotting as plot
+from multimds import plotting as plot
+from multimds import multimds as mm
 
-struct1 = dt.structure_from_file("ctrl_Scer_12_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12_32kb_structure.tsv")
+struct1, struct2 = mm.full_mds("ctrl_Scer_12_32kb.bed", "galactose_Scer_12_32kb.bed", weight=0, penalty=0.1)
 plot.plot_structures_interactive((struct1, struct2), out_path="sup6a.png")
 
-struct1 = dt.structure_from_file("ctrl_Scer_12-upstream_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12-upstream_32kb_structure.tsv")
+struct1, struct2 = mm.full_mds("ctrl_Scer_12-upstream_32kb.bed", "galactose_Scer_12-upstream_32kb.bed", weight=0, penalty=0.1)
 plot.plot_structures_interactive((struct1, struct2), out_path="sup6b_upstream.png")
 
-struct1 = dt.structure_from_file("ctrl_Scer_12-downstream_32kb_structure.tsv")
-struct2 = dt.structure_from_file("galactose_Scer_12-downstream_32kb_structure.tsv")
+struct1, struct2 = mm.full_mds("ctrl_Scer_12-downstream_32kb.bed", "galactose_Scer_12-downstream_32kb.bed", weight=0, penalty=0.1)
 plot.plot_structures_interactive((struct1, struct2), out_path="sup6b_downstream.png")
--- scripts/sup10.sh
+++ scripts/sup10.sh
@@ -2,7 +2,4 @@ set -e
 
 ./get_yeast_data.sh
 
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12_32kb.bed galactose_Scer_12_32kb.bed
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-upstream_32kb.bed galactose_Scer_12-upstream_32kb.bed
-python ../multimds.py -P 0.1 -w 0 ctrl_Scer_12-downstream_32kb.bed galactose_Scer_12-downstream_32kb.bed
 python sup10.py
