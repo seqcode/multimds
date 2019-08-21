b6f2e8fd727f3362a21811e0e03b187e1e5072d5
Lila <lur159@psu.edu>
2019-08-21 17:06:54 -0400

    update method calling

--- scripts/sup5.py
+++ scripts/sup5.py
@@ -1,13 +1,13 @@
 import numpy as np
-import sys
-sys.path.append("../multimds")
-import data_tools as dt
-import plotting as plot
+from multimds import plotting as plot
+from multimds import multimds as mm
 
 mappability = np.loadtxt("mappability_21_5kb.bed", usecols=3)
 
-struct1 = dt.structure_from_file("GM12878_combined_21_5kb_structure.tsv")
-struct2 = dt.structure_from_file("K562_21_5kb_structure.tsv")
+#struct1 = dt.structure_from_file("GM12878_combined_21_5kb_structure.tsv")
+#struct2 = dt.structure_from_file("K562_21_5kb_structure.tsv")
+
+struct1, struct2 = mm.full_mds("hic_data/GM12878_combined_21_5kb.bed", "hic_data/K562_21_5kb.bed")
 
 mappability = mappability[struct1.chrom.minPos/struct1.chrom.res + struct1.nonzero_abs_indices()]	#only loci in structures
 mappable = np.where(mappability > 0.75)
--- scripts/sup5.sh
+++ scripts/sup5.sh
@@ -5,6 +5,8 @@ do
 	./get_hic_data.sh $CELLTYPE 5000
 done
 
+./get_mappability.sh 5000
+
 if [ ! -e mappability_21_5kb.bed ]
 	then
 		cat mappability_5kb.bed | awk '$1 == "chr21" {print $0}' > mappability_21_5kb.bed
