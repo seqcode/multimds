2e399cc274046f45e83665c465067d17b64f3843
Lila <lur159@psu.edu>
2019-08-22 11:11:51 -0400

    merge scripts

--- scripts/sup21.sh
+++ scripts/sup21.sh
@@ -3,6 +3,9 @@ set -e
 RES=10000
 ./get_hic_data.sh GM12878_combined $RES
 ./get_hic_data.sh K562 $RES
-./relocalization_peaks_b_comp.sh $RES
 
+./relocalization_peaks_a_comp.sh $RES
+python sup21a.py
+
+./relocalization_peaks_b_comp.sh $RES
 python sup21b.py
