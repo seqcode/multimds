2b74c8f92b7146278bcd64f4deb4bb253a61566a
Lila <lur159@psu.edu>
2019-09-04 14:51:19 -0400

    update heatmap scripts

--- scripts/make_heatmap.py
+++ scripts/make_heatmap.py
@@ -1,6 +1,5 @@
 import sys
 from multimds import data_tools as dt
-sys.path.append("..")
 import heatmap as hm
 
 num = sys.argv[1]
@@ -10,8 +9,7 @@ size = dt.size_from_bed(path)
 struct = dt.structureFromBed(path, size)
 mat = dt.matFromBed(path, size, struct)
 
-#tads = np.loadtxt("sim{}_tads.tsv".format(num))
-#tad_indices = [(struct.get_rel_index(start), struct.get_rel_index(end)) for start,end in tads]
+tads = np.loadtxt("sim{}_tads.tsv".format(num))
+tad_indices = [(struct.get_rel_index(start), struct.get_rel_index(end)) for start,end in tads]
 
-#hm.heatMapFromMat(mat, maxvalue=50, tads=tad_indices)
-hm.heatMapFromMat(mat, maxvalue=50)
+hm.heatMapFromMat(mat, maxvalue=50, tads=tad_indices)
