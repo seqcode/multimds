6ace43760e894f5b4c043b89768eb4bccf3d4c97
Lila <lur159@psu.edu>
2019-09-26 15:50:57 -0400

    svg

--- scripts/fig6.py
+++ scripts/fig6.py
@@ -16,20 +16,20 @@ struct2 = dt.structure_from_file("{}_21_{}kb_structure.tsv".format(cell_type2, r
 gen_coords = np.array(struct1.getGenCoords())
 dists = np.array([la.calcDistance(coord1, coord2) for coord1, coord2 in zip(struct1.getCoords(), struct2.getCoords())])
 
-mat1 = dt.matFromBed("hic_data/{}_21_{}kb.bed".format(cell_type1, res_kb), struct1)
-comps1 = ca.get_compartments(mat1)
-mat2 = dt.matFromBed("hic_data/{}_21_{}kb.bed".format(cell_type2, res_kb), struct2)
-comps2 = ca.get_compartments(mat2)
+#mat1 = dt.matFromBed("hic_data/{}_21_{}kb.bed".format(cell_type1, res_kb), struct1)
+#comps1 = ca.get_compartments(mat1)
+#mat2 = dt.matFromBed("hic_data/{}_21_{}kb.bed".format(cell_type2, res_kb), struct2)
+#comps2 = ca.get_compartments(mat2)
 
-r, p = st.pearsonr(comps1, comps2)
-if r < 0:
-	comps1 = -comps1
+#r, p = st.pearsonr(comps1, comps2)
+#if r < 0:
+#	comps1 = -comps1
 
-comp_diffs = np.abs(comps1 - comps2)
+#comp_diffs = np.abs(comps1 - comps2)
 
 plt.subplot2grid((10,10), (0,0), 9, 10, frameon=False)
 plt.plot(gen_coords, dists/max(dists), lw=2, label="Relocalization", zorder=1)
-plt.plot(gen_coords, comp_diffs/max(comp_diffs), lw=2, label="Compartment score change", zorder=1)
+#plt.plot(gen_coords, comp_diffs/max(comp_diffs), lw=2, label="Compartment score change", zorder=1)
 
 plt.title("{} vs {}".format(cell_type1, cell_type2), fontsize=15)
 
@@ -63,4 +63,5 @@ plt.scatter([gen_coord], [0.005], c="r", s=100, marker="o", zorder=2)
 
 plt.legend(frameon=False)
 
-plt.savefig("{}_{}_{}kb_relocalization".format(cell_type1, cell_type2, res_kb))
+#plt.savefig("{}_{}_{}kb_relocalization".format(cell_type1, cell_type2, res_kb))
+plt.savefig("test_{}_{}_{}kb_relocalization.svg".format(cell_type1, cell_type2, res_kb))
