619cbf9c085908fce2740f1574c738cb7a770bb4
Lila <lur159@psu.edu>
2019-09-26 16:48:27 -0400

    svg

--- scripts/sup14.py
+++ scripts/sup14.py
@@ -93,4 +93,4 @@ plt.axhline(y=y_start, color="k", lw=4)
 #plot ticks
 plt.tick_params(direction="out", top=False, right=False, length=12, width=3, pad=5, labelsize=12)
 
-plt.savefig("sup14")
+plt.savefig("sup14.svg")
